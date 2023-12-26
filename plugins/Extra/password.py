e05030d25f5fbc7b959179335ba7c6c6fc9c222b
RB-Bots <89537060+RB-Bots@users.noreply.github.com>
2023-12-26 22:28:21 +0530

    Update password.py

--- plugins/Extra/password.py
+++ plugins/Extra/password.py
@@ -1,6 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 import random, os
 from pyrogram import Client, filters, enums 
@@ -19,5 +16,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('VJ Bots', url='https://t.me/vj_bots')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('RB Bots', url='https://t.me/RB_Backup')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
