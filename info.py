80e63038debcc1ad1cc860ebf6a2b09e7e05faed
RB-Bots <89537060+RB-Bots@users.noreply.github.com>
2024-01-03 16:51:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://govindrawat921:JJULgcKvXaTAfOa7@cluster0.nw68jo0.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://govindrawat921:Xi1NeuyKoDs7cJCm@cluster0.opoifmn.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "govindrawat921")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
