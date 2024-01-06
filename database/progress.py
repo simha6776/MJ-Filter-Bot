33013dfc9c27bb8ae1307c742c54ba223339bb65
RB-Bots <89537060+RB-Bots@users.noreply.github.com>
2024-01-06 06:49:31 +0530

    Update progress.py

--- database/progress.py
+++ database/progress.py
@@ -46,18 +46,18 @@ async def progress_for_pyrogram(
             pass
 
 
-def humanbytes(size):
+#def humanbytes(size):
     # https://stackoverflow.com/a/49361727/4723940
     # 2**10 = 1024
-    if not size:
-        return ""
-    power = 2**10
-    n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
-    while size > power:
-        size /= power
-        n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+   # if not size:
+  #      return ""
+  #  power = 2**10
+ #   n = 0
+ #   Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+#    while size > power:
+ #       size /= power
+#        n += 1
+#    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
 
 def TimeFormatter(milliseconds: int) -> str:
