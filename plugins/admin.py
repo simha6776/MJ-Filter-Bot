9322059a849f8368938e47191fbbf4081a44c718
RB-Bots <89537060+RB-Bots@users.noreply.github.com>
2024-01-06 08:35:03 +0530

    upload

---
+++ plugins/admin.py
@@ -0,0 +1,69 @@
+from pyrogram.types import (
+    InlineKeyboardButton, InlineKeyboardMarkup, ForceReply)
+import os
+from pyrogram import Client, filters
+from util.date import add_date
+from database.premiumdb import usertype, addpre
+ADMIN = int(os.environ.get("ADMIN", 1484670284))
+log_channel = int(os.environ.get("LOG_CHANNEL", ""))
+
+
+@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["warn"]))
+async def warn(c, m):
+        if len(m.command) >= 3:
+            try:
+                user_id = m.text.split(' ', 2)[1]
+                reason = m.text.split(' ', 2)[2]
+                await m.reply_text("User Notfied Sucessfully")
+                await c.send_message(chat_id=int(user_id), text=reason)
+            except:
+                 await m.reply_text("User Not Notfied Sucessfully 😔")
+
+
+@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["addpremium"]))
+async def buypremium(bot, message):
+	await message.reply_text("🦋 Select Plan to upgrade...", quote=True, reply_markup=InlineKeyboardMarkup([
+		           [
+				   InlineKeyboardButton("🪙 Silver", callback_data="vip1")
+				   ], [
+					InlineKeyboardButton("💫Gold", callback_data="vip2")
+				   ], [
+					InlineKeyboardButton("💎 Diamond", callback_data="vip3")
+					]]))
+
+        			
+@Client.on_callback_query(filters.regex('vip1'))
+async def vip1(bot,update):
+	id = update.message.reply_to_message.text.split("/addpremium")
+	user_id = id[1].replace(" ", "")
+	add_date  = date.today()
+	ex_date(int(user_id), today + timedelta(days=30)
+	usertype(int(user_id),"🪙 **SILVER**")
+	addpre(int(user_id))
+	await update.message.edit("Added successfully To Premium Upload limit 10 GB")
+	await bot.send_message(user_id,"Hey you are Upgraded To silver. check your plan here /myplan")
+	await bot.send_message(log_channel,f"⚡️ Plan Upgraded successfully 💥\n\nHey you are Upgraded To silver. check your plan here /myplan")
+
+@Client.on_callback_query(filters.regex('vip2'))
+async def vip2(bot,update):
+	id = update.message.reply_to_message.text.split("/addpremium")
+	user_id = id[1].replace(" ", "")
+    add_date  = date.today()
+	ex_date(int(user_id), today + timedelta(days=30)
+	usertype(int(user_id),"💫 **GOLD**")
+	addpre(int(user_id))
+	await update.message.edit("Added successfully To Premium Upload limit 50 GB")
+	await bot.send_message(user_id,"Hey you are Upgraded To Gold. check your plan here /myplan")
+
+@Client.on_callback_query(filters.regex('vip3'))
+async def vip3(bot,update):
+	id = update.message.reply_to_message.text.split("/addpremium")
+	user_id = id[1].replace(" ", "")
+	add_date  = date.today()
+	ex_date(int(user_id), today + timedelta(days=30)
+	usertype(int(user_id),"💎 **DIAMOND**")
+	addpre(int(user_id))
+	await update.message.edit("Added successfully To Premium Upload limit 100 GB")
+	await bot.send_message(user_id,"Hey you are Upgraded To Diamond. check your plan here /myplan")
+
+# CEASE POWER MODE @LAZYDEVELOPER
