55b18abc09e1f368dfc6ea0ed9fd227b78107cad
RB-Bots <89537060+RB-Bots@users.noreply.github.com>
2024-01-06 07:24:04 +0530

    upload

---
+++ database/premiumdb.py
@@ -0,0 +1,60 @@
+import pymongo
+import os
+from helper.date import add_date
+DB_NAME = os.environ.get("DB_NAME", "")
+DB_URL = os.environ.get("DB_URL", "")
+mongo = pymongo.MongoClient(DB_URL)
+db = mongo[DB_NAME]
+dbcol = db["user"]
+
+# Total User
+
+
+def total_user():
+    user = dbcol.count_documents({})
+    return user
+
+# insert bot Data
+
+# insert user data
+def insert(chat_id):
+    user_id = int(chat_id)
+    user_det = {"_id": user_id, "daily": 0,"usertype": "prexdate": None}
+                
+    try:
+        dbcol.insert_one(user_det)
+    except:
+        return True
+        pass
+
+
+def usertype(chat_id, type):
+    dbcol.update_one({"_id": chat_id}, {"$set": {"usertype": type}})
+
+
+def addpre(chat_id):
+    date = add_date()
+    dbcol.update_one({"_id": chat_id}, {"$set": {"prexdate": date[0]}})
+
+
+def addpredata(chat_id):
+    dbcol.update_one({"_id": chat_id}, {"$set": {"prexdate": None}})
+
+
+def daily(chat_id, date):
+    dbcol.update_one({"_id": chat_id}, {"$set": {"daily": date}})
+
+
+def getid():
+    values = []
+    for key in dbcol.find():
+        id = key["_id"]
+        values.append((id))
+    return values
+
+def delete(id):
+    dbcol.delete_one(id)
+
+
+def find_one(id):
+    return dbcol.find_one({"_id": id})
