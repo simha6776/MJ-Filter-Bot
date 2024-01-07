c2eb277d19818211d4a5fec8a18f4d3811b7d292
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-07 13:00:29 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -7,7 +7,7 @@ import secrets
 import time
 import mimetypes
 from aiohttp.http_exceptions import BadStatusLine
-from lazybot import multi_clients, work_loads, MJBot
+from MJbot import multi_clients, work_loads, MJBot
 from server.exceptions import FIleNotFound, InvalidHash
 from zzint import StartTime, __version__
 from util.custom_dl import ByteStreamer
