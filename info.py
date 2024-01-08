22ffeeccc0f0167e80c79228eb6d4a36a1aa3f4f
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-08 15:51:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -103,10 +103,10 @@ FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else 
 URL = "https://mj-test.onrender.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://mj-test.onrender.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
+WORKERS = int(environ.get('WORKERS', '10'))
+SESSION_NAME = str(environ.get('SESSION_NAME', 'MJBot'))
 MULTI_CLIENT = False
-name = str(environ.get('name', 'LazyPrincess'))
+name = str(environ.get('name', 'MJhitz'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
