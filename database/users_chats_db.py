7f7444ae250ddbe3ad6e2cd8dd64487a8dd1d3ff
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-08 14:51:17 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,12 +1,12 @@
 
 # https://github.com/odysseusmax/animated-lamp/blob/master/bot/database/database.py
-import pymongo
+import motor.motor_asyncio
 from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT, AUTO_DELETE, MAX_BTN, AUTO_FFILTER, SHORTLINK_API, SHORTLINK_URL, IS_SHORTLINK, TUTORIAL, IS_TUTORIAL
 
 class Database:
     
     def __init__(self, uri, database_name):
-        self._client = pymongo.MongoClient(uri)
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
         self.grp = self.db.groups
