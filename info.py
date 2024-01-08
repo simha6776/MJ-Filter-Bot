e1c92d40aa29254be72651c2f90a0523a0e2b2b8
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-08 15:47:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,7 +82,7 @@ MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
-LANGUAGES = ["Malayalam", "Marathi", "Tamil", "Telugu" ,"english", "hindi", "Bengali", "kannada", "Gujarati", "Punjabi", "Haryanvi", "Rajasthani"]
+LANGUAGES = ["kannada", "Tamil", "Telugu" ,"Malayalam", "hindi", "english", "Marathi", "Bengali", "Gujarati", "Punjabi", "Haryanvi", "Rajasthani"]
 
 SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "season 6" , "season 7" , "season 8" , "season 9" , "season 10"]
 
